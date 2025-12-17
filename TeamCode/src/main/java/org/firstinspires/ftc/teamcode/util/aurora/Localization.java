778156ca0bb7c6a8792bba21200dd760f7b00933
Caleb Hill <143536788+Imagineer7@users.noreply.github.com>
2025-12-17 13:37:12 -0900

    Added Localization blank

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/aurora/Localization.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.util.aurora;
+//This class will be for getting positioning and localization data for the robot.
+//It may use encoders, IMU, or other sensors to determine the robot's position on the field.
+//It will provide methods to get the robot's current coordinates and orientation.
+//No dependencies on other AURORA classes, except for AuroraHardwareConfig if necessary.
+
+//It will use the limelight to get position data when available, otherwise it will rely on odometry.
+//It will provide methods to reset position and update localization data.
+//Additionally, it may include methods for calculating distances to targets or waypoints on the field.
+public class Localization {
+}
